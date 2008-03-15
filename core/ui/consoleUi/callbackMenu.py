9f060146dbfbde234aa1b640bfbbdd9ca99a827b
aberezh <aberezh@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2008-03-15 16:21:13 +0000

    consoleUi: comments and license info.

--- core/ui/consoleUi/callbackMenu.py
+++ core/ui/consoleUi/callbackMenu.py
@@ -1,7 +1,36 @@
+'''
+callbackMenu.py
+
+Copyright 2008 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+
 from core.ui.consoleUi.history import *
 
 #TODO: extract a base class from this one and menu
 class callbackMenu:
+    '''
+    This is a menu-wrapper which delegates the command execution 
+    to the callback agent.
+    It provides the same interface as a menu,
+    but do not require the command line to be parsed by the console 
+    and do not provide any autocompletion for now.
+    '''
     
     def __init__(self, name, console, w3af, parent, callback, raw=True):
         self._name = name
--- core/ui/consoleUi/help.py
+++ core/ui/consoleUi/help.py
@@ -1,8 +1,35 @@
+'''
+help.py
+
+Copyright 2008 Andres Riancho
+
+This file is part of w3af, w3af.sourceforge.net .
+
+w3af is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation version 2 of the License.
+
+w3af is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with w3af; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+'''
+
 from string import Template
 import xml.etree.ElementTree as ET
 from xml.dom.minidom import *
 
 class helpRepository:
+    '''
+    This class wraps a help file and allows to extract 
+    context-related help objects
+    @author Alexander Berezhnoy (alexander.berezhnoy |at| gmail.com)
+    '''
     def __init__(self, path='core/ui/consoleUi/help.xml'):
         self.__doc = ET.parse(path)
         self.__map = {}
@@ -12,6 +39,15 @@ class helpRepository:
 
 
     def loadHelp(self, topic, obj=None, vars=None):
+        '''
+        Loads an object from the repository.
+        @parameter topic: the name of a context (for example, menu)
+        @parameter obj: the help object where to load the help data 
+        (if None, a new one is created)
+        @parameter vars: a dict of variables to replace in the help text
+        '''
+
+        #a closure to simplify the substitution
         def subst(templ):
             if not vars:
                 return templ
@@ -43,11 +79,14 @@ class helpRepository:
 
         return obj
 
+# main repository
 helpMainRepository = helpRepository()
 
     
-
 class help:
+    '''
+    Container for help items.
+    '''
     def __init__(self):
         self._table = {}
         self._subj2Gat = {}
@@ -55,6 +94,12 @@ class help:
 
 
     def addHelpEntry(self, subj, content, cat=''):
+        '''
+        Adds the help entry.
+        @parameter content: usually a tuple like (head, body)
+        @parameter cat: a name of the category. 
+        If the item exists in an other category, it will be replaced.
+        '''
 
         if type(content) not in (tuple, list): 
             content = (content, None)
@@ -90,6 +135,14 @@ class help:
 
 
     def getPlainHelpTable(self, separators=True, cat=None):
+        '''
+        Returns a table of format 'subject -> head' 
+        to display with the table.py module
+        @parameter separators: if True, the categories are separated 
+        by extra line.
+        @parameter cat: category to include into the page. 
+        If None, all are included.
+        '''
         result = []
         
         if cat is not None:
