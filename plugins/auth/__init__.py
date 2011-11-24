5d3d7b3f18bb5cc8c67f7a6319dbb83f4a2e2e96
oxdef <oxdef@16c29cf1-982c-0410-8ff8-8bb040e68b5b>
2011-11-24 18:25:00 +0000

    Added more detailed description

--- plugins/auth/__init__.py
+++ plugins/auth/__init__.py
@@ -3,5 +3,6 @@ def getLongDescription():
     '''
     @return: The description for the plugin type.
     '''    
-    return '''Auth plugin 
-    '''
+    return '''Auth plugins make possible to scan authorization protected web applications. 
+    They make login action in the beginning of the scan, logout - in the end 
+    and check current session action regularly.'''
